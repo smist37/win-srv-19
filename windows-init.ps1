e4eda02bc635a26f91c3328537fa635f73a0798e
smist37 <66137970+smist37@users.noreply.github.com>
2021-12-08 08:49:31 -0700

    Add files via upload
    Installs the PowerShell module to run Windows Updates, then updates Windows via PowerShell

---
+++ windows-init.ps1
@@ -0,0 +1,21 @@
+﻿Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+# Trust the PSGallery Repo so it will auto accept to Y
+
+Install-Module PSWindowsUpdate # Let powershell run Windows Update
+
+Get-WindowsUpdate # Run the Windows Update
+
+Write-Host "These are the following updates I'll download."
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot # Grab the latest Updates
+
+Write-Host "After 60 seconds the script will terminate and the system may reboot."
+
+$val = 60
+while ($val -ge 0) {
+Write-Host $val, "" -NoNewline;
+Start-Sleep -Seconds 1
+$val--;
+}
+
+Write-Host "All done."
